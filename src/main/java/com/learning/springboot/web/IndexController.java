0914c46bea78532926c19336e62cf8ca85d21c5b
Cat Hanbit <rmsdk9001@gmail.com>
2020-04-06 18:21:19 +0900

    ~CHAPTER 5

--- src/main/java/com/learning/springboot/web/IndexController.java
+++ src/main/java/com/learning/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.learning.springboot.web;
 
+import com.learning.springboot.config.auth.LoginUser;
+import com.learning.springboot.config.auth.dto.SessionUser;
 import com.learning.springboot.service.PostsService;
 import com.learning.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,8 +19,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
